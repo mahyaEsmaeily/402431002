14136f18204269a075b0b5f2f38f852893fff3c1
mahya <mahyaesmaily23@gmail.com>
2024-05-06 00:48:59 +0400

    mabnaye2

---
+++ mabnaye2/mabnaye2/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int main()
+{
+     int num;
+    cout << "Enter num:";
+    cin >> num;
+    vector<int> nums;
+    while ( num > 0 )
+    {
+        nums.push_back(num%2);
+
+        num=num/2;
+    }
+
+    for(int i = nums.size()-1;i>=0;i--)
+	{
+		cout << nums[i];
+	}
+}
